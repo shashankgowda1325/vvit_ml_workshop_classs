bdd294362ec6e1bd819f45d80bea4a985314853d
Shashank S <shashankgowda3835@gmail.com>
2026-03-18 12:10:45 +0530

    Update ml_taxi_app.py

--- ml_taxi_app.py
+++ ml_taxi_app.py
@@ -27,7 +27,7 @@ y = df['fare']
 X_train, X_test, y_train, y_test = train_test_split(
   X, y, test_size=0.2, random_state=42)
 
-model = Linear regression()
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
